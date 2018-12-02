eb03820c299c4e7068244d2a5810e1a9394cb978
Wisketchy Dobrov <warfolomey@gmail.com>
2018-12-02 22:55:34 +0300

    Create ipns_point_structure.py
    structure init (not for use in actual code)

---
+++ ipns_point_structure.py
@@ -0,0 +1,9 @@
+structure = {
+'Public_encryption': 'public Alice key for encoding', 
+'Public_signing': 'public Alice key for signing', 
+'Message': 
+	{'message_id_hash': #message_id_5383710b780583f1f462b6e719071523147cde25a5a47c380126c3395f84b9b3
+		{'Address': address,  #e6jB08WPeBjoPS4Lb4CFRVW/TAWSrGq7MD5OG8NVA794z4GwhlXz7vov1i2w6GLWYkY2vjhCgtux5vUuE+sKC97yMd9X9xhf5V8=
+		'Files': ['Qm...', 'Qm...', 'Qm...']}},
+    #... and other messages
+'Points': []}
