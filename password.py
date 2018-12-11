8feee3c83f5e307fae804c1cfc33a0078caf7c52
Wisketchy Dobrov <warfolomey@gmail.com>
2018-12-11 10:26:00 +0300

    make AES key from password

--- password.py
+++ password.py
@@ -1,5 +1,10 @@
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import PBKDF2
 import secrets as s
 import string
+
+from nacl.encoding import HexEncoder as benc
 
 def password_gen(type, length):
 	'''
@@ -30,6 +35,38 @@ def password_gen(type, length):
 		print('type:', type,'\n')
 		raise ValueError('Only "words" or "char" types are allowed.')	
 
+password = password_gen('words', 6)
+print(password)
+#print(password_gen('char', 25))
 
-print(password_gen('words', 6))
-print(password_gen('char', 25))
+
+def sha000(password, circles = 1):
+	'''
+	Some defence from rainbow tables
+	'''
+	for times in range(circles):
+		cond = 1
+		counter = 0
+		while cond:
+			sha256 = str(SHA256.new(password.encode()).hexdigest())
+			if sha256[0:4] == '0000':
+			#if sha256[0] == '0':
+				cond = 0	
+				result = sha256 
+				
+					
+			else:
+				password = sha256
+				counter += 1
+		password = result	
+	return result
+
+
+def make_aes_key(password, length):
+	password = sha000(password, 5)	
+	return PBKDF2(password, salt = (str(sha000(password, 2))), dkLen = length, count = 10000)
+
+key = make_aes_key(password, 32)
+cipher = AES.new(key, AES.MODE_EAX)
+print(benc.encode(key))
+
